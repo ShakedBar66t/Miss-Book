c802fa7e69c77d06257eb79dde576799f3795ba3
Shaked Barsheshet <alive2154@gmail.com>
2022-12-26 15:49:12 +0200

    added routing

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,19 @@
+const { Outlet, Link } = ReactRouterDOM
 
 export function About() {
 
     return <section className="about">
         <h3> welcome to our about page</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa distinctio a perferendis vitae, ad est ex fuga aliquam reprehenderit vero beatae fugiat, quam voluptate iure delectus commodi, repellat ullam accusantium!</p>
+
+        <nav>
+            <Link to="/about">Index</Link> |
+            <Link to="/about/team">Team</Link> |
+            <Link to="/about/vision">Vision</Link>
+        </nav>
+
+        <div className="nested-route">
+            <Outlet />
+        </div>
     </section>
 }
--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,5 +1,12 @@
 const { useState } = React
 
+const Router = ReactRouterDOM.HashRouter
+const { Route, Routes } = ReactRouterDOM
+
+import { AboutIndex } from "./cmps/about-index.jsx"
+import { AppHeader } from './cmps/app-header.jsx'
+import { Team } from "./cmps/team.jsx"
+import { Vision } from "./cmps/vision.jsx"
 import { Home } from './pages/home.jsx'
 import { About } from './pages/about.jsx'
 import { BookIndex } from './pages/book-index.jsx'
@@ -10,20 +17,23 @@ export function App() {
     const [page, setPage] = useState('book')
     console.log('page is', page)
 
-    return <section className="main-layout app">
-        <header className="app-header full main-layout">
-            <h1>Miss Book</h1>
-            <nav className="app-nav">
-                <a href="#" onClick={() => setPage('home')}>Home</a> |
-                <a href="#" onClick={() => setPage('about')}> About</a> |
-                <a href="#" onClick={() => setPage('book')}> Books</a>
-            </nav>
-        </header>
+    return <Router>
 
-        <main>
-            {page === 'home' && <Home/>}
-            {page === 'about' && <About/>}
-            {page === 'book' && <BookIndex/>}
-        </main>
-    </section>
+        <section className="main-layout app">
+
+            <AppHeader setPage={setPage} />
+
+            <main className="full main-layout">
+                <Routes>
+                    <Route element={<Home />} path="/" />
+                    <Route element={<About />} path="/about">
+                        <Route element={<AboutIndex />} path="/about" />
+                        <Route element={<Team />} path="/about/team" />
+                        <Route element={<Vision />} path="/about/vision" />
+                    </Route>
+                    <Route element={<BookIndex />} path="/book" />
+                </Routes>
+            </main>
+        </section>
+    </Router>
 }
